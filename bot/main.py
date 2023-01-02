6f7dfb99dc0093aa0b59ca20696985d9186b133b
vani5h <110791370+vani5h@users.noreply.github.com>
2023-01-01 20:04:17 -0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -111,7 +111,8 @@ async def on_message(message):
         await message.channel.send('idk gg ig') 
     formats = ['jpg', 'png', 'gif', 'svg', 'jpeg', 'imgur']
     attachments = [f for f in message.attachments if f.filename.split('.')[-1] in formats]
-    if message.author.id == 512433971037077519 and attachments:
+    embeds = message.Embed
+    if message.author.id == 512433971037077519 and attachments or embeds:
         await message.delete()
         await message.channel.send('furry tax')
     if "!!" in message.content:
