59b5f6cd900089e8245f7a83f83c10eb95b2b88f
vani5h <110791370+vani5h@users.noreply.github.com>
2023-01-10 21:10:18 -0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -111,14 +111,14 @@ async def on_message(message):
         await message.channel.send('nice slayer')
     if "orjiin unlocked Necron's Handle" in message.content:
         await message.channel.send('idk gg ig') 
-    formats = ['jpg', 'png', 'gif', 'svg', 'jpeg', 'imgur']
+    formats = ['jpg', 'png', 'gif', 'svg', 'jpeg', 'imgur', 'webp']
     attachments = [f for f in message.attachments if f.filename.split('.')[-1] in formats]
     if message.author.id == 259729734420791298 and attachments:
         await message.delete()
         await message.channel.send('furry tax')
     #links = ['imgur.com']
     #link = [word for word in message.content.lower() if word in links]
-    if message.author.id == 512433971037077519 and any(z in message.content for z in [
+    if message.author.id == 259729734420791298 and any(z in message.content for z in [
             "imgur.com", "https"
     ]):
         await message.delete()
