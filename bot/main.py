c6be34ebd8dbf699fbda573aeca47b6f334d11a5
vani5h <110791370+vani5h@users.noreply.github.com>
2023-12-13 18:40:57 -0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -17,6 +17,51 @@ intents.members = True
 client = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 get = discord.utils.get
 #conn = psycopg2.connect(DATABASE_URL, sslmode = 'require')
+from datetime import datetime
+
+def getDuration(then, now = datetime.now(), interval = "default"):
+
+    # Returns a duration as specified by variable interval
+    # Functions, except totalDuration, returns [quotient, remainder]
+
+    duration = now - then # For build-in functions
+    duration_in_s = duration.total_seconds() 
+    
+    def years():
+      return divmod(duration_in_s, 31536000) # Seconds in a year=31536000.
+
+    def days(seconds = None):
+      return divmod(seconds if seconds != None else duration_in_s, 86400) # Seconds in a day = 86400
+
+    def hours(seconds = None):
+      return divmod(seconds if seconds != None else duration_in_s, 3600) # Seconds in an hour = 3600
+
+    def minutes(seconds = None):
+      return divmod(seconds if seconds != None else duration_in_s, 60) # Seconds in a minute = 60
+
+    def seconds(seconds = None):
+      if seconds != None:
+        return divmod(seconds, 1)   
+      return duration_in_s
+
+    def totalDuration():
+        y = years()
+        d = days(y[1]) # Use remainder to calculate next variable
+        h = hours(d[1])
+        m = minutes(h[1])
+        s = seconds(m[1])
+
+        return "Time between dates: {} years, {} days, {} hours, {} minutes and {} seconds".format(int(y[0]), int(d[0]), int(h[0]), int(m[0]), int(s[0]))
+
+    return {
+        'years': int(years()[0]),
+        'days': int(days()[0]),
+        'hours': int(hours()[0]),
+        'minutes': int(minutes()[0]),
+        'seconds': int(seconds()),
+        'default': totalDuration()
+    }[interval]
+
 
 @client.event
 async def on_ready():
@@ -51,35 +96,11 @@ async def handle(ctx, user: discord.Member):
 @client.command(pass_context=True)
 async def duration(ctx):
     global handle
+    then = handle
     now = datetime.now()
-    duration = now - handle 
-    duration_in_s = duration.total_seconds()
-
-    def years():
-      return divmod(duration_in_s, 31536000) # Seconds in a year=31536000.
-
-    def days(seconds = None):
-      return divmod(seconds if seconds != None else duration_in_s, 86400) # Seconds in a day = 86400
-
-    def hours(seconds = None):
-      return divmod(seconds if seconds != None else duration_in_s, 3600) # Seconds in an hour = 3600
-
-    def minutes(seconds = None):
-      return divmod(seconds if seconds != None else duration_in_s, 60) # Seconds in a minute = 60
-
-    def seconds(seconds = None):
-      if seconds != None:
-        return divmod(seconds, 1)   
-      return duration_in_s
-
-    y = years()
-    d = days(y[1])
-    h = hours(d[1])
-    m = minutes(h[1])
-    s = seconds(m[1])
         
     await ctx.send(
-        f"it's been {} years, {} days, {} hours, {} minutes and {} seconds since handle".format(int(y[0]), int(d[0]), int(h[0]), int(m[0]), int(s[0]))
+        f"it's been {getDuration(then)} since a handle"
     )
 
 @client.command(pass_context=True)
