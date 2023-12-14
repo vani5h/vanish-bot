d7c8c3e1b93e996f27553ec38ccb17bf7769ea7f
vani5h <110791370+vani5h@users.noreply.github.com>
2023-12-13 18:19:05 -0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -42,7 +42,7 @@ async def handle(ctx, user: discord.Member):
                 handle = msg_2.created_at
                 #print(msg_time)
                 #created = datetime.strptime(created_1, "%d/%m/%Y %H:%M:%S")
-                await ctx.send(f"handle dropped at: {handle} UTC")
+                await ctx.send(f"handle dropped at: {handle.} UTC")
                 print(handle)
                 return
 
@@ -53,8 +53,33 @@ async def duration(ctx):
     global handle
     now = datetime.now()
     duration = now - handle 
+    duration_in_s = duration.total_seconds()
+
+    def years():
+      return divmod(duration_in_s, 31536000) # Seconds in a year=31536000.
+
+    def days(seconds = None):
+      return divmod(seconds if seconds != None else duration_in_s, 86400) # Seconds in a day = 86400
+
+    def hours(seconds = None):
+      return divmod(seconds if seconds != None else duration_in_s, 3600) # Seconds in an hour = 3600
+
+    def minutes(seconds = None):
+      return divmod(seconds if seconds != None else duration_in_s, 60) # Seconds in a minute = 60
+
+    def seconds(seconds = None):
+      if seconds != None:
+        return divmod(seconds, 1)   
+      return duration_in_s
+
+     y = years()
+     d = days(y[1])
+      h = hours(d[1])
+     m = minutes(h[1])
+     s = seconds(m[1])
+        
     await ctx.send(
-        f"it's been: {duration} since handle"
+        f"it's been {} years, {} days, {} hours, {} minutes and {} seconds since handle".format(int(y[0]), int(d[0]), int(h[0]), int(m[0]), int(s[0]))
     )
 
 @client.command(pass_context=True)
